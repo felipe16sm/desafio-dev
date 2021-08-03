bdb74636e3771ccc4acbca47e153b71e483da25a
Felipe Sobrinho Molina <felipe16sm@gmail.com>
2021-08-03 06:48:55 -0300

    feat: arquivos iniciais gerados pelo React.

---
+++ frontend-react-desafio-dev/src/App.js
@@ -0,0 +1,13 @@
+import { Routes } from "./Routes";
+import { NavBar } from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes />
+    </>
+  );
+}
+
+export default App;
