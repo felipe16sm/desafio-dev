17b69efabca6e78b4f2cc38043cb2a36253420e5
Felipe Sobrinho Molina <felipe16sm@gmail.com>
2021-08-03 06:46:22 -0300

    feat: implementação do componente que lista vários cards de lojas.

---
+++ frontend-react-desafio-dev/src/components/ListLojas/index.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { api } from "../../api";
+import { CardLoja } from "../CardLoja";
+import "./styles.css";
+
+export const ListLojas = () => {
+  const history = useHistory();
+  const [listLojasData, setlistLojasData] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("stores/")
+      .then((response) => {
+        console.log(response.data);
+        setlistLojasData(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }, []);
+
+  return (
+    <div>
+      {listLojasData.map((lojaData, index) => {
+        return <CardLoja lojaData={lojaData} key={index} />;
+      })}
+    </div>
+  );
+};
